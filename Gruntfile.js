bbde8e753a9e664c1fe89ccdae2f5ff1fd5c9450
Paula Molinero <pris54@gmail.com>
2016-07-17 23:57:37 +0100

    Update grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,24 @@
 var grunt = require('grunt');
 var shell = require('shelljs');
 
-grunt.registerTask('MongoDB', "Start MongoDB server", function() {
-    shell.exec('mongod --dbpath ./data');
+grunt.registerTask('mongodb', "Start MongoDB server", function() {
+    shell.exec('mongod --dbpath /Users/paula/WebServer/tableBooking/data');
 });
 
-grunt.registerTask('NodeJS', "Start node server", function() {
-    shell.exec('node server.js');
+grunt.registerTask('nodemon', "Start node server via nodemon", function() {
+    shell.exec('nodemon server.js');
 });
 
-grunt.registerTask('default', 'default task description', function(){
-    grunt.task.run('MongoDB');
-    grunt.task.run('NodeJS');
-    console.log('Running MongoDB and NodeJS server');
+grunt.initConfig({
+    concurrent: {
+        target: {
+            tasks: ['mongodb', 'nodemon']
+        },
+        options: {
+            logConcurrentOutput: true
+        }
+    }
 });
+
+grunt.loadNpmTasks('grunt-concurrent');
+grunt.registerTask('default', ['concurrent:target']);
